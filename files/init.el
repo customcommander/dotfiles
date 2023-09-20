0951a42eba8431781e43b354d652432985357ecb
customcommander <hello@spinjs.com>
2023-09-20 22:02:17 +0100

    fix indentation; add tree-sitter support

--- files/init.el
+++ files/init.el
@@ -15,6 +15,11 @@
 
 (setq windmove-wrap-around t)
 
+(electric-indent-mode -1)
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 2)
+(setq-default js-indent-level 2)
+
 (defvar bootstrap-version)
 
 (let ((bootstrap-file
@@ -34,9 +39,24 @@
 
 (straight-use-package 'use-package)
 
-(electric-indent-mode -1)
-(setq indent-tabs-mode nil)
-(setq tab-width 2)
+(setq treesit-language-source-alist
+   '((bash "https://github.com/tree-sitter/tree-sitter-bash")
+     (cmake "https://github.com/uyha/tree-sitter-cmake")
+     (css "https://github.com/tree-sitter/tree-sitter-css")
+     (elisp "https://github.com/Wilfred/tree-sitter-elisp")
+     (go "https://github.com/tree-sitter/tree-sitter-go")
+     (html "https://github.com/tree-sitter/tree-sitter-html")
+     (javascript "https://github.com/tree-sitter/tree-sitter-javascript" "master" "src")
+     (json "https://github.com/tree-sitter/tree-sitter-json")
+     (make "https://github.com/alemuller/tree-sitter-make")
+     (markdown "https://github.com/ikatyang/tree-sitter-markdown")
+     (python "https://github.com/tree-sitter/tree-sitter-python")
+     (toml "https://github.com/tree-sitter/tree-sitter-toml")
+     (tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
+     (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
+     (yaml "https://github.com/ikatyang/tree-sitter-yaml")))
+
+(mapc #'treesit-install-language-grammar (mapcar #'car treesit-language-source-alist))
 
 (use-package magit)
 
