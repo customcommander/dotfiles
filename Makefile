ba614b9427a200faf95a308c4ee4ba7faec95a27
customcommander <hello@spinjs.com>
2023-12-18 22:25:12 +0000

    add yabai and skhd

--- Makefile
+++ Makefile
@@ -1,7 +1,9 @@
 install: $(HOME)/.gitconfig \
          $(HOME)/.gitconfig-local \
          $(HOME)/.gitignore \
-         $(HOME)/.emacs.d/init.el
+         $(HOME)/.emacs.d/init.el \
+         $(HOME)/.yabairc \
+         $(HOME)/.skhdrc
 
 $(HOME)/.gitconfig: files/.gitconfig
 	cp -f $^ $@
@@ -9,6 +11,13 @@ $(HOME)/.gitconfig: files/.gitconfig
 $(HOME)/.gitconfig-local:; touch $(HOME)/.gitconfig-local
 
 $(HOME)/.gitignore: files/.gitignore
+	cp -f $^ $@
+
+$(HOME)/.yabairc: files/.yabairc
+	cp -f $^ $@
+	chmod +x $@
+
+$(HOME)/.skhdrc: files/.skhdrc
 	cp -f $^ $@
 
 $(HOME)/.emacs.d/init.el: files/init.el
--- files/init.el
+++ files/init.el
@@ -1,145 +1,147 @@
-(add-to-list 'initial-frame-alist '(fullscreen . maximized))
+  (add-to-list 'initial-frame-alist '(fullscreen . maximized))
 
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
+  (tool-bar-mode -1)
+  (scroll-bar-mode -1)
 
-(set-fringe-mode 0)
+  (set-fringe-mode 0)
 
-(global-display-line-numbers-mode t)
+  (global-display-line-numbers-mode t)
 
-(global-set-key (kbd "M-3") (lambda ()
-                              (interactive)
-                              (insert "#")))
+  (global-set-key (kbd "M-3") (lambda ()
+                                (interactive)
+                                (insert "#")))
 
-(windmove-default-keybindings)
+  (windmove-default-keybindings)
 
-(setq windmove-wrap-around t)
+  (setq windmove-wrap-around t)
 
-(electric-indent-mode -1)
-(setq-default indent-tabs-mode nil)
-(setq-default tab-width 2)
-(setq-default js-indent-level 2)
+  (electric-indent-mode -1)
+  (setq-default indent-tabs-mode nil)
+  (setq-default tab-width 2)
+  (setq-default js-indent-level 2)
 
-(setq read-process-output-max (* 1024 1024)) ;; 1mb
-(setq gc-cons-threshold 100000000)
+  (setq read-process-output-max (* 1024 1024)) ;; 1mb
+  (setq gc-cons-threshold 100000000)
 
-(defvar bootstrap-version)
+  (defvar bootstrap-version)
 
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 6))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
+  (let ((bootstrap-file
+         (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+        (bootstrap-version 6))
+    (unless (file-exists-p bootstrap-file)
+      (with-current-buffer
+          (url-retrieve-synchronously
+           "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+           'silent 'inhibit-cookies)
+        (goto-char (point-max))
+        (eval-print-last-sexp)))
+    (load bootstrap-file nil 'nomessage))
 
-;; tell `use-package` to use `straight.el` as the package manager
-(setq straight-use-package-by-default t)
+  ;; tell `use-package` to use `straight.el` as the package manager
+  (setq straight-use-package-by-default t)
 
-(straight-use-package 'use-package)
+  (straight-use-package 'use-package)
 
-(use-package ef-themes
-  :init (ef-themes-select 'ef-maris-dark))
+  (use-package ef-themes
+    :init (ef-themes-select 'ef-maris-dark))
 
-(setq treesit-language-source-alist
-   '((bash "https://github.com/tree-sitter/tree-sitter-bash")
-     (cmake "https://github.com/uyha/tree-sitter-cmake")
-     (css "https://github.com/tree-sitter/tree-sitter-css")
-     (elisp "https://github.com/Wilfred/tree-sitter-elisp")
-     (go "https://github.com/tree-sitter/tree-sitter-go")
-     (html "https://github.com/tree-sitter/tree-sitter-html")
-     (javascript "https://github.com/tree-sitter/tree-sitter-javascript" "master" "src")
-     (json "https://github.com/tree-sitter/tree-sitter-json")
-     (make "https://github.com/alemuller/tree-sitter-make")
-     (markdown "https://github.com/ikatyang/tree-sitter-markdown")
-     (python "https://github.com/tree-sitter/tree-sitter-python")
-     (toml "https://github.com/tree-sitter/tree-sitter-toml")
-     (tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
-     (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
-     (yaml "https://github.com/ikatyang/tree-sitter-yaml")))
+  (setq treesit-language-source-alist
+     '((bash "https://github.com/tree-sitter/tree-sitter-bash")
+       (cmake "https://github.com/uyha/tree-sitter-cmake")
+       (css "https://github.com/tree-sitter/tree-sitter-css")
+       (elisp "https://github.com/Wilfred/tree-sitter-elisp")
+       (go "https://github.com/tree-sitter/tree-sitter-go")
+       (html "https://github.com/tree-sitter/tree-sitter-html")
+       (javascript "https://github.com/tree-sitter/tree-sitter-javascript" "master" "src")
+       (json "https://github.com/tree-sitter/tree-sitter-json")
+       (make "https://github.com/alemuller/tree-sitter-make")
+       (markdown "https://github.com/ikatyang/tree-sitter-markdown")
+       (python "https://github.com/tree-sitter/tree-sitter-python")
+       (toml "https://github.com/tree-sitter/tree-sitter-toml")
+       (tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
+       (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
+       (yaml "https://github.com/ikatyang/tree-sitter-yaml")))
 
-(mapc #'treesit-install-language-grammar (mapcar #'car treesit-language-source-alist))
+  (mapc #'treesit-install-language-grammar (mapcar #'car treesit-language-source-alist))
 
-(use-package exec-path-from-shell
-  :init
-    (when (memq window-system '(mac ns x))
-      (exec-path-from-shell-initialize)))
+  (use-package exec-path-from-shell
+    :init
+      (when (memq window-system '(mac ns x))
+        (exec-path-from-shell-initialize)))
 
-(use-package magit)
+(setq org-src-preserve-indentation t)
 
-;; ivy and swipper are installed as dependencies of this package
-(use-package counsel
-  :commands (swiper-isearch
-             counsel-M-x
-             counsel-find-file
-             ivy-switch-buffer)
-  :init
-  (setq ivy-use-virtual-buffers t)
-  (setq ivy-count-format "(%d/%d)")
-  :config
-  (ivy-mode 1)
-  :bind (("C-s" . swiper-isearch)
-         ("M-x" . counsel-M-x)
-         ("C-x C-f" . counsel-find-file)
-         ("C-x b" . ivy-switch-buffer)))
+  (use-package magit)
 
-(use-package company
-  :hook ((after-init . global-company-mode)))
+  ;; ivy and swipper are installed as dependencies of this package
+  (use-package counsel
+    :commands (swiper-isearch
+               counsel-M-x
+               counsel-find-file
+               ivy-switch-buffer)
+    :init
+    (setq ivy-use-virtual-buffers t)
+    (setq ivy-count-format "(%d/%d)")
+    :config
+    (ivy-mode 1)
+    :bind (("C-s" . swiper-isearch)
+           ("M-x" . counsel-M-x)
+           ("C-x C-f" . counsel-find-file)
+           ("C-x b" . ivy-switch-buffer)))
 
-(use-package ag)
+  (use-package company
+    :hook ((after-init . global-company-mode)))
 
-;; IMPORTANT: language servers must be installed separately!
-;; e.g. clojure-lsp can be installed via brew
-(use-package lsp-mode
-  :init
-  (setq lsp-keymap-prefix "s-l")
-  (setq lsp-headerline-breadcrumb-enable nil)
-  :hook ((clojure-mode . lsp-deferred)
-         (clojurescript-mode . lsp-deferred)
-         (js-mode . lsp-deferred))
-  :commands (lsp lsp-deferred))
+  (use-package ag)
 
-(use-package lsp-ivy)
+  ;; IMPORTANT: language servers must be installed separately!
+  ;; e.g. clojure-lsp can be installed via brew
+  (use-package lsp-mode
+    :init
+    (setq lsp-keymap-prefix "s-l")
+    (setq lsp-headerline-breadcrumb-enable nil)
+    :hook ((clojure-mode . lsp-deferred)
+           (clojurescript-mode . lsp-deferred)
+           (js-mode . lsp-deferred))
+    :commands (lsp lsp-deferred))
 
-(use-package projectile
-  :config
-  (projectile-mode +1)
-  (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
-  (setq projectile-project-search-path '(("~/GitHub" . 1)
-                                         ("~/Code" . 1)))
-  (setq projectile-completion-system 'ivy))
+  (use-package lsp-ivy)
 
-(use-package add-node-modules-path
-  :hook ((js-mode . #'add-node-modules-path)
-         (tsx-ts-mode . #'add-node-modules-path)))
+  (use-package projectile
+    :config
+    (projectile-mode +1)
+    (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+    (setq projectile-project-search-path '(("~/GitHub" . 1)
+                                           ("~/Code" . 1)))
+    (setq projectile-completion-system 'ivy))
 
-(use-package prettier-js
-  :hook ((js-mode . prettier-js-mode)
-         (tsx-ts-mode . prettier-js-mode)))
+  (use-package add-node-modules-path
+    :hook ((js-mode . #'add-node-modules-path)
+           (tsx-ts-mode . #'add-node-modules-path)))
 
-(use-package cider)
+  (use-package prettier-js
+    :hook ((js-mode . prettier-js-mode)
+           (tsx-ts-mode . prettier-js-mode)))
 
-(use-package clojure-mode
-  :mode (("\\.cljc?\\'" . clojure-mode)
-         ("\\.cljs\\'" . clojurescript-mode)))
+  (use-package cider)
 
-(use-package rainbow-delimiters
-  :hook ((clojure-mode . rainbow-delimiters-mode)
-         (clojurescript-mode . rainbow-delimiters-mode)
-         (emacs-lisp-mode . rainbow-delimiters-mode)))
+  (use-package clojure-mode
+    :mode (("\\.cljc?\\'" . clojure-mode)
+           ("\\.cljs\\'" . clojurescript-mode)))
 
-(use-package paredit
-  :hook ((clojure-mode . paredit-mode)
-         (clojurescript-mode . paredit-mode)
-         (emacs-lisp-mode . paredit-mode)))
+  (use-package rainbow-delimiters
+    :hook ((clojure-mode . rainbow-delimiters-mode)
+           (clojurescript-mode . rainbow-delimiters-mode)
+           (emacs-lisp-mode . rainbow-delimiters-mode)))
 
-(use-package janet-mode
-  :mode (("\\.janet\\'" . janet-mode))
-  :hook ((janet-mode . paredit-mode)
-         (janet-mode . rainbow-delimiters-mode)))
+  (use-package paredit
+    :hook ((clojure-mode . paredit-mode)
+           (clojurescript-mode . paredit-mode)
+           (emacs-lisp-mode . paredit-mode)))
 
-(use-package restclient)
+  (use-package janet-mode
+    :mode (("\\.janet\\'" . janet-mode))
+    :hook ((janet-mode . paredit-mode)
+           (janet-mode . rainbow-delimiters-mode)))
+
+  (use-package restclient)
