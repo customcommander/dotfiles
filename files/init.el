24d6f060cacb0e4cc4898b7f1b7939d848f9c982
customcommander <hello@spinjs.com>
2024-11-11 08:12:03 +0000

    add lsp and make it work for tsx-ts-mode

--- files/init.el
+++ files/init.el
@@ -95,22 +95,32 @@
 
   (setq treesit-language-source-alist
      '((bash "https://github.com/tree-sitter/tree-sitter-bash")
-       (cmake "https://github.com/uyha/tree-sitter-cmake")
        (css "https://github.com/tree-sitter/tree-sitter-css")
        (elisp "https://github.com/Wilfred/tree-sitter-elisp")
-       (go "https://github.com/tree-sitter/tree-sitter-go")
        (html "https://github.com/tree-sitter/tree-sitter-html")
        (javascript "https://github.com/tree-sitter/tree-sitter-javascript" "master" "src")
        (json "https://github.com/tree-sitter/tree-sitter-json")
        (make "https://github.com/alemuller/tree-sitter-make")
        (markdown "https://github.com/ikatyang/tree-sitter-markdown")
        (python "https://github.com/tree-sitter/tree-sitter-python")
-       (toml "https://github.com/tree-sitter/tree-sitter-toml")
        (tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
        (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
        (yaml "https://github.com/ikatyang/tree-sitter-yaml")))
 
   (mapc #'treesit-install-language-grammar (mapcar #'car treesit-language-source-alist))
+
+(use-package lsp-mode
+  :commands (lsp lsp-deferred)
+  :init
+  (setq lsp-keymap-prefix "C-c l")
+  :config
+  (lsp-enable-which-key-integration t))
+
+(use-package corfu
+  :init
+  (global-corfu-mode)
+  :config
+  (setq corfu-auto t))
 
   (use-package exec-path-from-shell
     :init
@@ -142,7 +152,9 @@
 
 (use-package wgrep)
 
-(add-to-list 'auto-mode-alist '("\\.[jt]sx?" . tsx-ts-mode))
+(add-to-list 'auto-mode-alist '("\\.[jt]s[x]?\\'" . tsx-ts-mode))
+
+(add-hook 'tsx-ts-mode-hook 'lsp-deferred)
 
   (use-package add-node-modules-path
     :hook ((tsx-ts-mode . #'add-node-modules-path)))
